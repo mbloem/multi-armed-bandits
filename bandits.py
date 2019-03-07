884ca45bcee8c5ba7ab17ea53e9fc947d63d9862
Michael Bloem <mbloem@mosaicdatascience.com>
2019-03-07 15:30:53 -0500

    Now with a random strategy, refactored stat-keeping, and regret stats.

--- bandits.py
+++ bandits.py
@@ -70,9 +70,15 @@ class MABPolicy( object ):
         self.N = 0
         self.choices = []
         self.score = []
+        self.regret = []
 
     def sample( self ):
         raise NotImplementedError('MABPolicy must implement the sample method')
+
+    def update_stats( self , k, choice, result, ):
+        self.wins[ choice ] += result
+        self.trials[ choice ] += 1
+        self.N += 1
 
 class BayesianStrategy( MABPolicy ):
     """
@@ -107,23 +113,24 @@ class BayesianStrategy( MABPolicy ):
         
         score = np.zeros( n )
         choices = np.zeros( n )
+        regret = np.zeros( n )
         
         for k in range(n):
-            #sample from the bandits's priors, and select the largest sample
-            choice = np.argmax( rbeta( 1 + self.wins, 1 + self.trials - self.wins) )
+            # sample from the bandits's priors, and select the largest sample
+            choice = np.argmax( rbeta( 1 + self.wins, 1 + self.trials - self.wins ) )
             
-            #sample the chosen bandit
+            # sample the chosen bandit
             result = self.bandits.pull( choice )
             
-            #update priors and score
-            self.wins[ choice ] += result
-            self.trials[ choice ] += 1
-            score[ k ] = result 
-            self.N += 1
+            # update stats (which updates priors for next pull)
+            self.update_stats( k, choice, result )
+            score[ k ] = result
             choices[ k ] = choice
+            regret[ k ] = self.bandits.p[ self.bandits.optimal ] - self.bandits.p[ choice ]
             
         self.score = np.r_[ self.score, score ]
         self.choices = np.r_[ self.choices, choices ]
+        self.regret = np.r_[ self.regret, regret ]
         return 
 
 class epsilonGreedyStrategy( MABPolicy ):
@@ -158,27 +165,29 @@ class epsilonGreedyStrategy( MABPolicy ):
     def sample( self, n=1 ):
         score = np.zeros( n )
         choices = np.zeros( n )
+        regret = np.zeros( n )
         
         for k in range(n):
-            #sample from the bandits's priors, and select the largest sample
+            # Choose action using an epsilon-greedy agent
             choice = self.get_action()
             
-            #sample the chosen bandit
+            # sample the chosen bandit
             result = self.bandits.pull( choice )
             
-            #update stats and Q action-value
-            self.wins[ choice ] += result
-            self.trials[ choice ] += 1
+            # update stats
+            self.update_stats( k, choice, result )
+            score[ k ] = result
+            choices[ k ] = choice
+            regret[ k ] = self.bandits.p[ self.bandits.optimal ] - self.bandits.p[ choice ]
+
             # Update Q action-value using:
             # Q(a) <- Q(a) + 1/(k+1) * (r(a) - Q(a))
-            # note that self.trials[ choice ] has been incremented to k+1 above
+            # note that self.trials[ choice ] has been incremented to k+1 in update_stats above
             self.Q[ choice ] += (1./self.trials[ choice ]) * (float(result) - self.Q[ choice ])
-            score[ k ] = result 
-            self.N += 1
-            choices[ k ] = choice
             
         self.score = np.r_[ self.score, score ]
         self.choices = np.r_[ self.choices, choices ]
+        self.regret = np.r_[ self.regret, regret ]
         return
 
     # Choose action using an epsilon-greedy agent
@@ -188,6 +197,53 @@ class epsilonGreedyStrategy( MABPolicy ):
             action_explore = np.random.randint( len(self.bandits) )  # explore random bandit
             return action_explore
         else:
-            #randomly select from among the bandits with the current maximum Q value
+            # randomly select from among the bandits with the current maximum Q value
             action_greedy = np.random.choice(np.flatnonzero(self.Q == self.Q.max()))
             return action_greedy
+
+class RandomStrategy( MABPolicy ):
+    """
+    Implements a random strategy for
+    the Multi-Armed Bandit problem.
+
+    Sub-class of MABPolicy base class.
+    
+    parameters:
+        bandits: a Bandit class with .pull method
+    
+    methods:
+        sample_bandits(n): sample and train on n pulls.
+
+    attributes:
+        N: the cumulative number of samples
+        choices: the historical choices as a (N,) array
+        score: the historical score as a (N,) array
+
+    """
+    def __init__(self, bandits):
+        
+        MABPolicy.__init__(self, bandits)
+
+    def sample( self, n=1 ):
+        
+        score = np.zeros( n )
+        choices = np.zeros( n )
+        regret = np.zeros( n )
+        
+        for k in range(n):
+            # random action
+            choice = np.random.randint( len(self.bandits) )
+            
+            # sample the chosen bandit
+            result = self.bandits.pull( choice )
+            
+            # update stats
+            self.update_stats( k, choice, result )
+            score[ k ] = result
+            choices[ k ] = choice
+            regret[ k ] = self.bandits.p[ self.bandits.optimal ] - self.bandits.p[ choice ]
+            
+        self.score = np.r_[ self.score, score ]
+        self.choices = np.r_[ self.choices, choices ]
+        self.regret = np.r_[ self.regret, regret ]
+        return 
